b861d6142dce8e14e606c2f0a49f516742420b2d
Douglas dos Santos Dias <douglas_tq@hotmail.com>
2019-07-02 18:13:38 -0300

    Envio Inicial do App produzido no Visual Studio 2019 em Xamarin.forms

---
+++ AppCorreios/AppCorreios/MainPage.xaml.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using AppCorreios.Servico.Modelo;
+using AppCorreios.Servico;
+namespace AppCorreios
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            BOTAO.Clicked += BuscarCEP;
+        }
+
+        private void BuscarCEP(object sender, EventArgs args)
+        {
+            string cep = CEP.Text.Trim();
+            if (isValidCEP(cep))
+            {
+                try
+                {
+                    Endereco end = ViaCEPServico.BuscarEnderecoViaCEP(cep);
+                    if (end != null)
+                    {
+
+                        RESULTADO.Text = string.Format("Endereço: {2} de {3}, {0}, {1}", end.localidade, end.uf, end.logradouro, end.bairro);
+                    }
+                    else
+                    {
+                        string infCEP = string.Format("O endereço não foi encontrado pelo CEP informado: {0}", cep);
+
+                        DisplayAlert("ERRO", infCEP, "OK");
+
+                    }
+                }
+                catch(Exception e)
+                {
+                    DisplayAlert("ERRO CRÍTICO", e.Message, "OK");
+
+                }
+            }
+
+        }
+
+        private bool isValidCEP(string cep)
+        {
+
+            bool valido = true;
+            if (cep.Length != 8)
+            {
+                //erro
+                DisplayAlert("ERRO", "CEP Inválido! O CEP deve conter 8 carecteres.", "OK");
+
+                valido = false;
+            }
+            int NovoCEp = 0;
+            if (!int.TryParse(cep, out NovoCEp))
+            {
+                //erro
+                DisplayAlert("ERRO", "CEP Inválido! O CEP deve ser composto apenas por números.", "OK");
+
+                valido = false;
+            }
+            return valido;
+        }
+    }
+}
---
+++ AppCorreios/AppCorreios/Servico/ViaCEPServico.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using AppCorreios.Servico.Modelo;
+using Newtonsoft.Json;
+
+namespace AppCorreios.Servico
+{
+    public class ViaCEPServico
+    {
+        private static string EnderecoURl = "https://viacep.com.br/ws/{0}/json/";
+
+        public static Endereco BuscarEnderecoViaCEP(string cep)
+        {
+            string NovoEnderecoURL = string.Format(EnderecoURl, cep);
+
+            WebClient wc = new WebClient();
+
+            string Conteudo = wc.DownloadString(NovoEnderecoURL);
+
+            Endereco end = JsonConvert.DeserializeObject<Endereco>(Conteudo);
+
+            if (end.cep == null) return null;
+
+            return end;
+
+        }
+    }
+}
